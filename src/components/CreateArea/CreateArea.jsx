81b09a022d327d9cc417609e2dd2aa4c9ff4a64f
theo-abhi <theo.abhi2793@gmail.com>
2025-07-17 17:49:21 +0530

    Update CreateArea.jsx

--- src/components/CreateArea/CreateArea.jsx
+++ src/components/CreateArea/CreateArea.jsx
@@ -1,11 +1,20 @@
 import "./CreateArea.css";
 import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
 
 function CreateArea(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
+
+  const handleExpand = () => {
+    setIsExpanded(true);
+  };
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -18,32 +27,38 @@ function CreateArea(props) {
     });
   }
 
+  function submitNote(event) {
+    event.preventDefault();
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+  }
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          props.onAdd(note);
-          setNote({
-            title: "",
-            content: "",
-          });
-        }}
-      >
-        <input
-          name="title"
-          placeholder="Title"
-          onChange={handleChange}
-          value={note.title}
-        />
+      <form className="create-note" onSubmit={submitNote}>
+        {isExpanded && (
+          <input
+            name="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={note.title}
+          />
+        )}
         <textarea
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
           onChange={handleChange}
+          onClick={handleExpand}
           value={note.content}
         />
-        <button type="submit">Add</button>
+        <Zoom in={isExpanded}>
+          <Fab type="submit">
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
